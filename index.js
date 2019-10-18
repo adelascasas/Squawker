3f93f56aa3ac50b815aec9ed6b81a82e8b579e04
Ubuntu <ubuntu@nodeapp.cloud.compas.cs.stonybrook.edu>
2019-10-18 20:36:11 +0000

    added endpoints

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req,res) => {
+       res.send("Hello server1");
+});
+
+app.post("/adduser",(req,res) => {
+    res.send("Hello");
+});
+
+app.post("/login",(req,res) => {
+    res.send("Hello");
+});
+
+app.post("/logout",(req,res) => {
+    res.send("Hello");
+});
+
+app.post("/verify",(req,res) => {
+    res.send("Hello");
+});
+
+
+app.listen(5000,"192.168.122.19");
