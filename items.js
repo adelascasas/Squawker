69aa00c784e478e2d0e9bc94f04909a8036e5dda
Mike Spadafora <miikekm@gmail.com>
2019-11-13 21:52:40 -0500

    fixed one single request type from like to post

--- items.js
+++ items.js
@@ -2,7 +2,7 @@ const express = require('express');
 const database = require('./database');
 const router = express.Router();
 
-router.like('/item/:id/like',(req,res) => {
+router.post('/item/:id/like',(req,res) => {
     if (typeof req.body.like === 'undefined' || req.body.like === true){
         database.incrementLikes(req.params.id).then((result => {
             res.status(200).json({status: 'OK'});
